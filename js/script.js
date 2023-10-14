380913b25f503bf1ad2d1a4d4bba1351b8d0c9a4
Jeffrey Magallanes Jr <79354016+JeffreyMagallanesJr@users.noreply.github.com>
2023-10-14 20:39:49 +0800

    Add Hover Turn Black Effect

--- js/script.js
+++ js/script.js
@@ -3,8 +3,11 @@ const container = document.querySelector('.container');
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const div = document.createElement('div');
-        div.style.border = '1px solid black';
-        div.style.flex = `0 0 ${(500 - (2 * 16)) / 16}px`;
+        div.style.flex = `1 0 ${(500) / 16}px`;
+
+        div.addEventListener('mouseover', function() {
+            div.style.backgroundColor = 'black';
+        })
 
         container.appendChild(div);
     }
